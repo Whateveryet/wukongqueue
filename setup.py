9dbf93fbe4a4e1d9a62d828d3dc9c021a9a79601
lilei <random2035@qq.com>
2020-01-01 13:25:29 +0800

    change instruction

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from os import path
+
 from setuptools import setup, find_packages
+
 from wukongqueue import __version__
+
 DIR = path.dirname(path.abspath(__file__))
 
 with open(path.join(DIR, "README.md")) as f:
@@ -14,8 +17,7 @@ setup(
     version=__version__,
     author="chaseSpace",
     author_email="randomlilei@gmail.com",
-    description="A small and convenient cross process FIFO queue "
-    "service based on TCP protocol",
+    description="A lightweight and convenient cross process FIFO queue service based on TCP protocol.",
     keywords="cross process queue",
     url="https://github.com/chaseSpace/WukongQueue",
     long_description=README,
