3aff9324e5e56d280bbbc9a0618c18d2e1c48c0e
lilei <random2035@qq.com>
2020-02-11 10:36:48 +0800

    change import

--- tests/server_tests.py
+++ tests/server_tests.py
@@ -2,7 +2,10 @@
 import sys,logging
 from unittest import TestCase, main
 sys.path.append("../")
-from wukongqueue import *
+try:
+    from wukongqueue.wukongqueue import *
+except ImportError:
+    from wukongqueue import *
 
 max_size = 2
 host = "127.0.0.1"
